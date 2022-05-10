1e1418154b544a697b5dabaa7cafe73a00302c24
ofirshtrosberg <72875939+ofirshtrosberg@users.noreply.github.com>
2022-05-10 22:33:08 +0300

    Add files via upload

---
+++ ex2_client.c
@@ -0,0 +1,69 @@
+// אופיר שטרוסברג, 207828641
+// לידור דנון, 211823240
+#include <sys/random.h>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+void handler5(){
+    printf("end of stage: server returned the solution\n");
+    char fileName[100] = {};
+    sprintf(fileName, "toClient%d.txt", getpid());
+    int fdToClient = open(fileName, O_RDONLY);
+    int readBytes;
+    char ch;
+    if(fdToClient == -1){
+        printf("ERROR_FROM_EX2\n");
+        remove(fileName);
+        exit(-1);
+    }
+    printf("sol = ");
+    do{
+       readBytes = read(fdToClient,&ch,1);
+       if(readBytes == 1){
+       printf("%c", ch);
+       }
+    }while(readBytes == 1);
+    printf("\n");
+    printf("end of stage: client finished printing the solution\n");
+    close(fdToClient);
+    remove(fileName);
+    exit(0);
+}
+int main(int argc, char * argv[]){
+    signal(5, handler5);
+    if(argc!=5){
+        printf("ERROR_FROM_EX2\n");
+        exit(-1);
+    }
+    int i;
+    unsigned int randNum;
+    int toServerfd;
+    for(i=0;i<10;i++){
+        toServerfd = open("to_srv.txt", O_CREAT | O_WRONLY | O_TRUNC, 0666);
+        if(toServerfd == -1){
+            getrandom(&randNum,sizeof(unsigned int), GRND_NONBLOCK);
+            randNum = randNum%5 + 1;
+            sleep(randNum);
+        }
+        else{
+            char buffer[700] = {};
+            sprintf(buffer,"%d\n%s\n%s\n%s\n" , getpid(),argv[2],argv[3],argv[4]);
+            write(toServerfd, &buffer, strlen(buffer));
+            close(toServerfd);
+            if(kill(atoi(argv[1]),5)==-1){
+                printf("ERROR_FROM_EX2\n");
+                remove("to_srv.txt");
+                exit(-1);
+            }
+            sleep(10);
+            exit(0);
+        }
+    }
+    printf("ERROR_FROM_EX2\n");
+    remove("to_srv.txt");
+    exit(-1);
+}
---
+++ ex2_srv.c
@@ -0,0 +1,149 @@
+// אופיר שטרוסברג, 207828641
+// לידור דנון, 211823240
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+int timer = 60;
+void handler5(){
+        signal(5, handler5);
+        printf("\nend of stage: client conected to server\n");
+        int result;
+        pid_t pid;
+        int status;
+        int toclientfd;
+        char *line1 = NULL;
+        char *line2 = NULL;
+        char *line3 = NULL;
+        char *line4 = NULL;
+        size_t  len1 = 0;
+        size_t  len2 = 0;
+        size_t  len3 = 0;
+        size_t  len4 = 0;
+        int flagError = 0;
+        char stringToWrite[700] = {};
+        if((pid = fork()) == 0){
+                FILE *clientStream = fopen("to_srv.txt", "r");
+                if (clientStream == NULL)
+                {
+                        printf("ERROR_FROM_EX2\n");
+                        flagError = 1;
+                }
+                if (getline(&line1, &len1, clientStream) <= 0)
+                {
+                        printf("ERROR_FROM_EX2\n");
+                        free(line1);
+                        fclose(clientStream);
+                        flagError = 1;
+                }
+                if (getline(&line2, &len2, clientStream) <= 0)
+                {
+                        printf("ERROR_FROM_EX2\n");
+                        free(line1);
+                        free(line2);
+                        fclose(clientStream);
+                        flagError = 1;
+                }
+                if (getline(&line3, &len3, clientStream) <= 0)
+                {
+                        printf("ERROR_FROM_EX2\n");
+                        free(line1);
+                        free(line2);
+                        free(line3);
+                        fclose(clientStream);
+                        flagError = 1;
+                }
+                if (getline(&line4, &len4, clientStream) <= 0)
+                {
+                        printf("ERROR_FROM_EX2\n");
+                        free(line1);
+                        free(line2);
+                        free(line3);
+                        free(line4);
+                        fclose(clientStream);
+                        flagError = 1;
+                }
+                fclose(clientStream);
+                line1[strcspn(line1, "\n")] = 0;
+                line2[strcspn(line2, "\n")] = 0;
+                line3[strcspn(line3, "\n")] = 0;
+                line4[strcspn(line4, "\n")] = 0;
+                //need atoi for all what we read
+                char fileName2[100] = {};
+                sprintf(fileName2, "toClient%s.txt", line1);
+                toclientfd = open(fileName2, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+                int firstNum = atoi(line2);
+                int op = atoi(line3);
+                int secondNum = atoi(line4);
+                if (secondNum == 0 && op == 4){
+                        sprintf(stringToWrite, "cannot devide by zero");
+                        write(toclientfd, &stringToWrite, strlen(stringToWrite));
+                        close(toclientfd);
+                        flagError = 1;
+                        if(kill(atoi(line1), 5)==-1){
+                                printf("ERROR_FROM_EX2\n");
+                                flagError = 1;
+                        }
+                        else{
+                            printf("end of stage: server finished computing the solution\n");    
+                        }
+                        
+                }
+                else{
+                        if(op == 1){
+                                result = firstNum + secondNum;
+                        }
+                        else if(op == 2){
+                                result = firstNum - secondNum;
+                        }
+                        else if(op == 3){
+                                result = firstNum * secondNum;
+                        }
+                        else if(op == 4){
+                                result = firstNum / secondNum;
+                        }
+                        else{
+                                sprintf(stringToWrite, "invalid operation");
+                                write(toclientfd, &stringToWrite, strlen(stringToWrite));
+                                close(toclientfd);
+                                flagError = 1;
+                                if(kill(atoi(line1), 5)==-1){
+                                        printf("ERROR_FROM_EX2\n");
+                                        flagError = 1;
+                                }
+                                else{
+                                        printf("end of stage: server finished computing the solution\n");
+                                }
+                        }
+                }
+        if(flagError == 0){
+                sprintf(stringToWrite, "%d", result);
+                // printf("string to write %s\n", stringToWrite);
+                write(toclientfd, &stringToWrite, strlen(stringToWrite));
+                close(toclientfd);
+                if(kill(atoi(line1), 5)==-1){
+                        printf("ERROR_FROM_EX2\n");
+                        flagError = 1;
+                }
+                else{
+                        printf("end of stage: server finished computing the solution\n");
+                }
+        }
+        remove("to_srv.txt");
+        timer = 60;
+        raise(9);
+        } else {
+        wait(&status);
+        }
+}
+int main(){
+        signal(5, handler5);
+        while(timer > 0){
+                sleep(1);
+                timer--;
+        }
+        exit(0);
+}
